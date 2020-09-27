a6ba6c1a0b2cf3ebebede45b080696c8a8c5128d
tkkawa <tkm.kawa@gmail.com>
2020-09-27 18:52:41 +0900

    update Number of Students Doing Homework at a Given Time

--- 1450_number-of-students-doing-homework-at-a-given-time.py
+++ 1450_number-of-students-doing-homework-at-a-given-time.py
@@ -1,7 +1,3 @@
 class Solution:
     def busyStudent(self, startTime: List[int], endTime: List[int], queryTime: int) -> int:
-        ret = 0
-        for i in range(len(startTime)):
-            if startTime[i] <= queryTime <= endTime[i]:
-                ret += 1
-        return ret
+        return sum([st <= queryTime <= et for st, et in zip(startTime, endTime)])
