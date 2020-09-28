df9c39d39642a5e0fa552b46518f5d98e7cbce53
tkkawa <tkm.kawa@gmail.com>
2020-09-28 21:56:30 +0900

    update Find N Unique Integers Sum up to Zero

--- 1304_find-n-unique-integers-sum-up-to-zero.py
+++ 1304_find-n-unique-integers-sum-up-to-zero.py
@@ -1,7 +1,6 @@
 import numpy as np
 class Solution:
     def sumZero(self, n: int) -> List[int]:
-        ret = []
         ret = np.arange(-(n//2), n//2+1).tolist()
         if n % 2 == 0:
             ret.pop(len(ret)//2)
