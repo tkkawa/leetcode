852343960bbcee95f5557c661cf1c6fabd54ae6d
tkkawa <tkm.kawa@gmail.com>
2020-10-13 13:07:46 +0900

    update Can Make Arithmetic Progression From Sequence

--- 1502_can-make-arithmetic-progression-from-sequence.py
+++ 1502_can-make-arithmetic-progression-from-sequence.py
@@ -1,9 +1,8 @@
 class Solution:
     def canMakeArithmeticProgression(self, arr: List[int]) -> bool:
-        arr = sorted(arr)
-        diff = arr[1] - arr[0]
-        for i in range(len(arr)-1):
-            if arr[i+1] - arr[i] != diff:
+        arr.sort()
+        diff = arr[0] - arr[1]
+        for ai, aj in zip(arr, arr[1:]):
+            if diff != ai - aj:
                 return False
-            diff = arr[i+1] - arr[i]
         return True
