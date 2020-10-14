c05458e09199faae14a15812fff67a6ed9043e67
tkkawa <tkm.kawa@gmail.com>
2020-10-15 00:46:45 +0900

    update Sum of Nodes with Even-Valued Grandparent

--- 1315_sum-of-nodes-with-even-valued-grandparent.py
+++ 1315_sum-of-nodes-with-even-valued-grandparent.py
@@ -1,32 +1,22 @@
-# v = node num
-# time : O(v)
-# space : O(v)
-# Definition for a binary tree node.
+# V = node num
+# time: O(V)
+# space: O(V)
 from collections import deque
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def sumEvenGrandparent(self, root: TreeNode) -> int:
-        stack = deque([root])
+        stack = deque([(None, None, root)])
         ret = 0
-        if root is None:
-            return 0
         while stack:
-            root = stack.popleft()
-            if root.val % 2 == 0:
-                if root.left:
-                    ret += root.left.left.val if root.left.left else 0
-                    ret += root.left.right.val if root.left.right else 0
-                if root.right:
-                    ret += root.right.left.val if root.right.left else 0
-                    ret += root.right.right.val if root.right.right else 0
-            if root.right is None and root.left is None:
+            gp, p, n = stack.pop()
+            if n is None:
                 continue
-            if root.left:
-                stack.append(root.left)
-            if root.right:
-                stack.append(root.right)
+            ret += (gp is not None and gp.val % 2 == 0) * n.val
+            stack.append((p, n, n.left))
+            stack.append((p, n, n.right))
         return ret
