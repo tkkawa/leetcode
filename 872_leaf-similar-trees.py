3d53232a0b520b50d3f8ca51d4908eff529f88ef
tkkawa <tkm.kawa@gmail.com>
2020-10-15 00:44:59 +0900

    update Leaf-Similar Trees

--- 872_leaf-similar-trees.py
+++ 872_leaf-similar-trees.py
@@ -1,34 +1,25 @@
-# v = node num
-# time : O(v)
-# space : O(v)
+# V = max{node num in tree1, node num in tree2}
+# time: O(V)
+# space: O(V)
+
 # Definition for a binary tree node.
-from collections import deque
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def makeLeaflist(self, root):
-        ret = []
-        stack =deque([root])
-        if root is None:
-            return 0
-        while stack:
-            root = stack.pop()
-            if root.right is None and root.left is None:
-                ret.append(root.val)
-                continue
-            if root.right:
-                stack.append(root.right)
-            if root.left:
-                stack.append(root.left)
-        return ret
-    
-    
     def leafSimilar(self, root1: TreeNode, root2: TreeNode) -> bool:
-        ret1 = self.makeLeaflist(root1)
-        ret2 = self.makeLeaflist(root2)
-        print(ret1)
-        print(ret2)
-        return ret1 == ret2
+        def traverse(leaves, n):
+            if n is None:
+                return
+            if n.left is None and n.right is None:
+                leaves.append(n.val)
+                return
+            traverse(leaves, n.left)
+            traverse(leaves, n.right)
+            
+        l1, l2 = [], []
+        traverse(l1, root1)
+        traverse(l2, root2)
+        return l1 == l2
