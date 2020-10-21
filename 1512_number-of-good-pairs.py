5c80b3cbe3b8abfd20762031fa991f4a776aec81
tkkawa <tkm.kawa@gmail.com>
2020-10-21 19:23:58 +0900

    update 1512 Number of Good Pairs

--- 1512_number-of-good-pairs.py
+++ 1512_number-of-good-pairs.py
@@ -1,21 +1,13 @@
-# n = nums_set.length
-# m = nums.count(i)
-# time : O(n*m)
-# space : O(n)
+# N = len(nums)
+# time: O(N)
+# space: O(N)
+
+from collections import defaultdict
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
-        def kaijo(n, k=2):
-            if n == 1:
-                return n
-            return n * kaijo(n-1)
-        nums_set = list(set(nums))
+        ref = defaultdict(int)
         ret = 0
-        for i in nums_set:
-            if nums.count(i) == 1:
-                continue
-            if nums.count(i) == 2:
-                ret += 1
-                continue
-            ret += kaijo(nums.count(i))/(kaijo(nums.count(i) - 2) * 2)
-            
-        return int(ret)
+        for n in nums:
+            ret += ref[n]
+            ref[n] += 1
+        return ret
