7ff339e46cd4a2da5f434b3d070e5449a376596d
tkkawa <tkm.kawa@gmail.com>
2020-10-22 18:06:22 +0900

    add 965 Univalued Binary Tree

---
+++ 965_univalued-binary-tree.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+from collections import deque
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def isUnivalTree(self, root: TreeNode) -> bool:
+        if root is None:
+            return True
+        if root.left is None and root.right is None:
+            return True
+        stack = deque([root])
+        while stack:
+            root = stack.popleft()
+            if root.left is None and root.right is None:
+                continue
+            if root.left:
+                stack.append(root.left)
+                if root.val != root.left.val:
+                    return False
+            if root.right:
+                stack.append(root.right)
+                if root.val != root.right.val:
+                    return False
+        return True
