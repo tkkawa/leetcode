733df3ad41717636b7607ce2735de3bc7f7ff8bc
tkkawa <tkm.kawa@gmail.com>
2020-11-02 21:00:01 +0900

    add 1409 Queries on a Permutation With Key

---
+++ 1409_queries-on-a-permutation-with-key.py
@@ -0,0 +1,13 @@
+# n = queries.length
+# time : O(n^2)
+# space : O(m)
+class Solution:
+    def processQueries(self, queries: List[int], m: int) -> List[int]:
+        P = [i for i in range(1, m+1)]
+        ret = []
+        for que in queries:
+            ind = P.index(que)
+            ret.append(ind)
+            P.pop(ind)
+            P.insert(0, que)
+        return ret
