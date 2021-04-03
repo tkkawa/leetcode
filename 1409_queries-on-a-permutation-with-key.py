3abd4691506e57087bad3bf38747018fc92db394
tkkawa <tkm.kawa@gmail.com>
2021-04-03 22:01:24 +0900

    update 1409 Queries on a Permutation With Key

--- 1409_queries-on-a-permutation-with-key.py
+++ 1409_queries-on-a-permutation-with-key.py
@@ -1,9 +1,9 @@
 # n = queries.length
 # time : O(n^2)
-# space : O(n+m)
+# space : O(m)
 class Solution:
     def processQueries(self, queries: List[int], m: int) -> List[int]:
-        P = [i for i in range(1, m+1)]
+        P = list(range(1, m+1))
         ret = []
         for que in queries:
             ind = P.index(que)
