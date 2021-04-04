6631e45fb66b589479c1386e2fe5170e1cea44a4
tkkawa <tkm.kawa@gmail.com>
2021-04-04 18:41:06 +0900

    update 415 Add Strings

--- 415_add-strings.py
+++ 415_add-strings.py
@@ -1,26 +1,21 @@
-# n = min(num1.length, num2.length)
-# m = max(num1,length, num2.length)
+# n = max(num2dd1.length, num2.length)
 # time : O(n)
-# space : O(m)
+# space : O(1)
 class Solution:
     def addStrings(self, num1: str, num2: str) -> str:
-        num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-        lis1 = list(num1)[::-1]
-        lis2 = list(num2)[::-1]
-        res = []
-        a = 0
-        for i in range(min(len(lis1), len(lis2))):
-            res.append(num.index(lis1[i]) + num.index(lis2[i]))
-            if i == len(lis1)-1:
-                lis = lis2
-            if i == len(lis2)-1:
-                lis = lis1
-        if len(lis) != len(res):
-            for i in range(len(res), len(res)+max(len(lis1), len(lis2))-min(len(lis1), len(lis2))):
-                res.append(num.index(lis[i]))
-        a = 1
-        sum = 0
-        for i in range(len(res)):
-            sum += a * res[i]
-            a *= 10
-        return str(sum)
+        total1 = ord(num1[0])-48
+        for i in num1[1:]:
+            num = ord(i)-48
+            total1 = total1*10+num
+        total2 = ord(num2[0])-48
+        for i in num2[1:]:
+            num = ord(i)-48
+            total2 = total2*10+num
+        total = total1+total2
+        if total == 0:
+            return "0"
+        text = ""
+        while total != 0:
+            total, remain = divmod(total,10)
+            text += (chr(remain+48))
+        return text[::-1]
