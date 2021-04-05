c796c147f967910c9fc8402a0e1d4d3896381316
tkkawa <tkm.kawa@gmail.com>
2021-04-05 22:08:19 +0900

    update 1013 Partition Array Into Three Parts With Equal Sum

--- 1013_partition-array-into-three-parts-with-equal-sum.py
+++ 1013_partition-array-into-three-parts-with-equal-sum.py
@@ -3,7 +3,9 @@
 # space = O(1)
 class Solution:
     def canThreePartsEqualSum(self, arr: List[int]) -> bool:
-        sub_sum = sum(arr) / 3
+        sub_sum, m = divmod(sum(arr), 3)
+        if m != 0:
+            return False
         sub = 0
         a = 0
         for num in arr:
