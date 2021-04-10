d1f77e6567ff2cc71525a0360c45550bcd7c39ac
tkkawa <tkm.kawa@gmail.com>
2021-04-10 21:39:11 +0900

    update 67. Add Binary

--- 67_add-binary.py
+++ 67_add-binary.py
@@ -3,9 +3,7 @@
 # space = O(1)
 # done time = 10m
 
-
 class Solution:
+
     def addBinary(self, a: str, b: str) -> str:
-        if a == "0" and b == "0":
-            return "0"
-        return bin(int(a, 2) + int(b, 2)).lstrip('0b')
+        return bin(int(a, 2) + int(b, 2))[2: ]
