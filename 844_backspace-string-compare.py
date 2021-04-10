876c66178eaffe0a4176c48d6e2d12a6c1f2a9b6
tkkawa <tkm.kawa@gmail.com>
2021-04-10 21:11:29 +0900

    update 844 Backspace String Compare

--- 844_backspace-string-compare.py
+++ 844_backspace-string-compare.py
@@ -1,25 +1,19 @@
 # n = max(s.length, t.length)
-# time = O(n^2)
-# space = O(1)
+# time = O(n)
+# space = O(n)
 # done time = 30m
 
 
 class Solution:
 
-    def backspaceCompare(self, s: str, t: str) -> bool:
-        def backspace(moji, n):
-            i = 0
-            while i < n:
-                if moji[i] == "#":
-                    if i == 0:
-                        moji = moji[1:]
-                        i -= 1
-                        n -= 1
-                    else:
-                        moji = moji[:i-1] + moji[i+1:]
-                        i -= 2
-                        n -= 2
-                i += 1
-            return moji
-
-        return backspace(s, len(s)) == backspace(t, len(t))
+    def backspaceCompare(self, S: str, T: str) -> bool:
+        def convert(s):
+            ret = []
+            for c in s:
+                if c == '#':
+                    if ret:
+                        ret.pop()
+                else:
+                    ret.append(c)
+            return ret
+        return convert(S) == convert(T)
