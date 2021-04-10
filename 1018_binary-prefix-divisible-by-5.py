0794bb32ae6aab08cc6adeaa7702afc6da253513
tkkawa <tkm.kawa@gmail.com>
2021-04-10 21:34:13 +0900

    add 1018. Binary Prefix Divisible By 5

--- 1018_binary-prefix-divisible-by-5.py
+++ 1018_binary-prefix-divisible-by-5.py
@@ -1,16 +1,15 @@
 # n = A.length
-# time = O(n^2)
+# time = O(n)
 # space = O(n)
 # done time = 30m
+
 class Solution:
+
     def prefixesDivBy5(self, A: List[int]) -> List[bool]:
-        for i in range(len(A)):
-            A[i] = str(A[i])
-        A.reverse()
-        res = []
-        for i in range(len(A)):
-            if int(''.join(A[i:][::-1]), 2)%5 == 0:
-                res.append(True)
-            else:
-                res.append(False)
-        return res[::-1]
+        num = 0
+        ret = []
+        for a in A:
+            num = (num << 1) + a
+            ret.append(num % 5 == 0)
+
+        return ret
