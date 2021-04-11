aada77dc438cf35a60eaf1b736fb67a1765ab118
tkkawa <tkm.kawa@gmail.com>
2021-04-11 22:09:30 +0900

    update 415 Add Strings

--- 415_add-strings.py
+++ 415_add-strings.py
@@ -1,21 +1,25 @@
 # n = max(num2dd1.length, num2.length)
 # time : O(n)
 # space : O(1)
+
+
 class Solution:
+
     def addStrings(self, num1: str, num2: str) -> str:
-        total1 = ord(num1[0])-48
+        zero_code = ord("0")
+        total1 = ord(num1[0]) - zero_code
         for i in num1[1:]:
-            num = ord(i)-48
-            total1 = total1*10+num
-        total2 = ord(num2[0])-48
+            num = ord(i) - zero_code
+            total1 = total1 * 10 + num
+        total2 = ord(num2[0]) - zero_code
         for i in num2[1:]:
-            num = ord(i)-48
-            total2 = total2*10+num
-        total = total1+total2
+            num = ord(i) - zero_code
+            total2 = total2 * 10+num
+        total = total1 + total2
         if total == 0:
             return "0"
         text = ""
         while total != 0:
-            total, remain = divmod(total,10)
-            text += (chr(remain+48))
+            total, remain = divmod(total, 10)
+            text += (chr(remain + zero_code))
         return text[::-1]
