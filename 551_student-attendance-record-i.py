4dd064c3cc4084173ce31f66e1a77fb430fcfa56
tkkawa <tkm.kawa@gmail.com>
2021-04-18 19:41:04 +0900

    update 551 Student Attendance Record I

--- 551_student-attendance-record-i.py
+++ 551_student-attendance-record-i.py
@@ -7,17 +7,4 @@
 class Solution:
 
     def checkRecord(self, s: str) -> bool:
-        if len(s) - len(s.replace('A', '')) >= 2:
-            return False
-        L_conti_count = 1
-        late = 'L'
-        late_limit = 3
-        for i in range(1, len(s)):
-            if s[i] == late and s[i] == s[i-1]:
-                L_conti_count += 1
-            else:
-                if L_conti_count >= late_limit - 1:
-                    L_conti_count = 1
-            if L_conti_count >= late_limit:
-                return False
-        return True
+        return not(1 < s.count('A') or 'LLL' in s)
