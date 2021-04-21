38bc48d334752171e7dbe962783b41196a18dd38
tkkawa <tkm.kawa@gmail.com>
2021-04-21 23:51:39 +0900

    update 989. Add to Array-Form of Integer

--- 989_add-to-array-form-of-integer.py
+++ 989_add-to-array-form-of-integer.py
@@ -1,17 +1,21 @@
 # n = nums.length
 # time = O(n)
-# space = O(n)
+# space = O(1)
 # done time = 15m
 
 
 class Solution:
 
     def addToArrayForm(self, nums: List[int], k: int) -> List[int]:
-        num = ""
-        for n in nums:
-            num += str(n)
-        ret_num = str(int(num) + k)
-        ret = []
-        for n in ret_num:
-            ret.append(int(n))
-        return ret
+        nums[-1] += k
+        digit_num = len(nums) - 1
+        one_digit_max = 9
+        while 0 < digit_num and one_digit_max < nums[digit_num]:
+            div, mod = divmod(nums[digit_num], one_digit_max + 1)
+            nums[digit_num-1] += div
+            nums[digit_num] = mod
+            digit_num -= 1
+        while one_digit_max < nums[0]:
+            nums = [nums[0] // (one_digit_max + 1)] + nums
+            nums[1] = nums[1] % (one_digit_max + 1)
+        return nums
