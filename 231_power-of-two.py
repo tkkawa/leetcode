4fd598e3eaaa6813875e965ccfdff4e1b1f977e3
tkkawa <tkm.kawa@gmail.com>
2021-04-21 23:02:13 +0900

    add 231. Power of Two

---
+++ 231_power-of-two.py
@@ -0,0 +1,17 @@
+# n = n
+# time = O(n)
+# space = O(1)
+# done time = 15m
+
+
+class Solution:
+
+    def isPowerOfTwo(self, n: int) -> bool:
+        if n <= 0:
+            return False
+        MOD = 2
+        while 1 < n:
+            n, mod = divmod(n, MOD)
+            if mod != 0:
+                return False
+        return True
