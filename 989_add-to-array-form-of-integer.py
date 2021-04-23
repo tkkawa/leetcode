7d741fb63e5ceb029cb32a35f05adf7be28c03e2
tkkawa <tkm.kawa@gmail.com>
2021-04-23 19:40:28 +0900

    update 989 Add to Array-Form of Integer

--- 989_add-to-array-form-of-integer.py
+++ 989_add-to-array-form-of-integer.py
@@ -7,15 +7,4 @@
 class Solution:
 
     def addToArrayForm(self, nums: List[int], k: int) -> List[int]:
-        nums[-1] += k
-        digit_num = len(nums) - 1
-        one_digit_max = 9
-        while 0 < digit_num and one_digit_max < nums[digit_num]:
-            div, mod = divmod(nums[digit_num], one_digit_max + 1)
-            nums[digit_num-1] += div
-            nums[digit_num] = mod
-            digit_num -= 1
-        while one_digit_max < nums[0]:
-            nums = [nums[0] // (one_digit_max + 1)] + nums
-            nums[1] = nums[1] % (one_digit_max + 1)
-        return nums
+        return [int(n) for n in str(int(''.join(map(str, A))) + K)]
