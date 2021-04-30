2d06685f25690b8b6de59c7f0af06fcbfdfa20c4
tkkawa <tkm.kawa@gmail.com>
2021-04-30 23:20:32 +0900

    update 459. Repeated Substring Pattern

--- 459_repeated-substring-pattern.py
+++ 459_repeated-substring-pattern.py
@@ -1,14 +1,28 @@
 # n = s.length
-# time = O(n)
-# space = O(1)
+# time = O(nlogn)
+# space = O(n)
 # done time = 15m
 
 
 class Solution:
 
     def repeatedSubstringPattern(self, s: str) -> bool:
-        for i in range(1, len(s) // 2 + 1):
-            if s[:i]*(len(s) // i) == s:
-                return True
+        def make_divisors(n):
+            lower_divisors, upper_divisors = [], []
+            divisor = 1
+            while divisor * divisor <= n:
+                if n % divisor == 0:
+                    lower_divisors.append(i)
+                    if divisor != n // divisor:
+                        upper_divisors.append(n//divisor)
+                divisor += 1
+            return lower_divisors + upper_divisors[::-1]
 
-        return False
+        N = len(s)
+        divs = make_divisors(N)
+
+        for d in divs:
+            if d == N:
+                return False
+            if s[: d] * (N // d) == s:
+                return True
