7c89f2651bc3e36151759ad0d11c70f1bac7fd04
tkkawa <tkm.kawa@gmail.com>
2021-05-03 19:35:52 +0900

    update 1232. Check If It Is a Straight Line

--- 1232_check-if-it-is-a-straight-line.py
+++ 1232_check-if-it-is-a-straight-line.py
@@ -1,13 +1,12 @@
 # n = coordinates.length
-# time = O(nlogn)
+# time = O(n)
 # space = O(1)
 # done time = 30m
 
 
 class Solution:
 
-    def checkStraightLine(self, coordinates: List[List[int]]) -> bool:
-        arr = sorted(coordinates, key=lambda x: x[0])
+    def checkStraightLine(self, arr: List[List[int]]) -> bool:
         N = len(arr)
         return all(
             (arr[i + 1][1] - arr[i][1]) * (arr[i + 2][0] - arr[i + 1][0]) ==
