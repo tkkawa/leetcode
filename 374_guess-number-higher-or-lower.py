81876958a0dd1093172e5998a8300a91d0680e8e
tkkawa <tkm.kawa@gmail.com>
2021-05-03 19:15:12 +0900

    update 374. Guess Number Higher or Lower

--- 374_guess-number-higher-or-lower.py
+++ 374_guess-number-higher-or-lower.py
@@ -14,11 +14,11 @@ class Solution:
         high = n
         mid = (low + high) >> 1
         while low < high:
-            guess_low_high = guess(mid)
-            if guess_low_high == -1:
+            return_val_by_guess = guess(mid)
+            if return_val_by_guess == -1:
                 high = mid
                 mid = (low + high) >> 1
-            elif guess_low_high == 1:
+            elif return_val_by_guess == 1:
                 low = mid
                 mid = (low + high) >> 1
             else:
